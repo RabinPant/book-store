44ad48bf44c72efdc627784d3f5ba868a0d5cde5
RabinPant <pantrabin12@gmail.com>
2019-09-23 23:10:57 -0700

    added header component and gave logo and design for header com

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 
-
+import Header from './components/header/header.component.jsx';
 function App() {
   return (
     <div>
+      <Header/>
       <switch>
         <Route exact path = '/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
