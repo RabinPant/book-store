d2453ac530719fc14a84bc4de0cb781574feed2c
RabinPant <pantrabin12@gmail.com>
2019-11-11 13:38:44 -0800

    implemented firebse utlis, ability of auth

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 
 import Header from './components/header/header.component';
 
-import { auth } from './firebase/firebase.utlis';
+import { auth, createUserProfileDocument } from './firebase/firebase.utlis';
 
 class  App extends React.Component {
   constructor(){
@@ -26,11 +26,23 @@ class  App extends React.Component {
 
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user=>{
-      
-      this.setState({currentUser:user});
-      
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth=>{
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        
+        userRef.onSnapshot(snapShot=>{
+          this.setState({
+            currentUser:{
+              id:snapShot.id,
+              ...snapShot.data()
+            }
+          })
+          console.log(this.state);
+        });
+      }
+      else{
+        this.setState({currentUser:userAuth});  
+      }
     });
   }
 
